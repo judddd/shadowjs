84770c743af7d99820d3c49b17fa325575caab5d
xuhao <535315941@qq.com>
2024-06-04 14:25:45 +0800

    xc

--- chatamz.js
+++ chatamz.js
@@ -1,8 +1,7 @@
 /*
-
 [rewrite_local]
 
-^https:\/\/api\.ai520\.co\/chatapi\/member\/wallet url script-response-body https://github.com/judddd/shadowjs/blob/main/chatamz.js
+^https:\/\/api\.ai520\.co\/chatapi\/member\/wallet url script-response-body https://raw.githubusercontent.com/judddd/shadowjs/main/chatamz.js
 
 [mitm]
 
