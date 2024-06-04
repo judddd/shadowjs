74b7cbdd0a06d0ee280f98f2f9764a9020b810ee
xuhao <535315941@qq.com>
2024-06-04 15:27:56 +0800

    123

--- chatamz.js
+++ chatamz.js
@@ -12,111 +12,6 @@ var url = $request.url;
 
 if ($response.body) {
     let responseBody = $response.body;
-    if (url.includes("https://api.ai520.co/chatapi/auth/memberInfo")) {
-       var objc = JSON.parse($response.body);
-       objc= {
-    "code": 200,
-    "extras": null,
-    "message": "",
-    "result": {
-        "account": "130****1796",
-        "authorizes": [],
-        "avatar": null,
-        "contact": "<p>遇到问题需要协助？扫码可联系客服寻求帮助！</p><p><img src=\"https://s2.loli.net/2023/05/05/YsXa32Lo7w6fOWQ.jpg\" alt=\"\" width=\"150\" height=\"149\" /></p>",
-        "email": null,
-        "groupInfo": {
-            "expVal": 10,
-            "iconUrl": "https://api.ai520.co/upload/2023/09/28/14549763653573.png",
-            "id": 13922195697093,
-            "privileges": [
-                {
-                    "aiModels": null,
-                    "customAIModels": null,
-                    "levelPrivilegType": 2,
-                    "levelPrivilegTypeName": "基础功能",
-                    "value": null
-                },
-                {
-                    "aiModels": [
-                        10,
-                        12,
-                        13,
-                        20,
-                        21,
-                        22,
-                        23,
-                        24
-                    ],
-                    "customAIModels": null,
-                    "levelPrivilegType": 100,
-                    "levelPrivilegTypeName": "基础对话模型",
-                    "value": -0.7
-                },
-                {
-                    "aiModels": null,
-                    "customAIModels": null,
-                    "levelPrivilegType": 200,
-                    "levelPrivilegTypeName": "画画Relax模式",
-                    "value": null
-                },
-                {
-                    "aiModels": null,
-                    "customAIModels": null,
-                    "levelPrivilegType": 3,
-                    "levelPrivilegTypeName": "画廊",
-                    "value": null
-                },
-                {
-                    "aiModels": null,
-                    "customAIModels": null,
-                    "levelPrivilegType": 4,
-                    "levelPrivilegTypeName": "写作",
-                    "value": null
-                },
-                {
-                    "aiModels": null,
-                    "customAIModels": null,
-                    "levelPrivilegType": 5,
-                    "levelPrivilegTypeName": "思维导图",
-                    "value": null
-                },
-                {
-                    "aiModels": null,
-                    "customAIModels": null,
-                    "levelPrivilegType": 202,
-                    "levelPrivilegTypeName": "画画图生文",
-                    "value": null
-                }
-            ],
-            "title": "黄金会员"
-        },
-        "inviteCode": "GZA88P4RN",
-        "isVerify": true,
-        "nickName": "M_NVyVmvYW",
-        "phone": "130****1796",
-        "timeCards": [],
-        "wallets": [
-            {
-                "availableValue": 11,
-                "memberWalletType": 1,
-                "totalValue": 11
-            },
-            {
-                "availableValue": 695,
-                "memberWalletType": 10,
-                "totalValue": 5500
-            },
-            {
-                "availableValue": 28,
-                "memberWalletType": 20,
-                "totalValue": 28
-            }
-        ]
-    },
-    "type": "success"
-}  
-       $done({ body: JSON.stringify(objc) });
-    } else {
     responseBody = responseBody.replace(/("availableValue"\s*:\s*)\d+/g, '$16666');
     
     let responseObject = JSON.parse(responseBody);
@@ -124,5 +19,5 @@ if ($response.body) {
 
      
     $done({ body: JSON.stringify(responseObject) });
-    }
+    
 }
