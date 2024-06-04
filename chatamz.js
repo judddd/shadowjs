3f09b17f603c2ea9d5f849eb8f147fffc06d2e2c
xuhao <535315941@qq.com>
2024-06-04 09:56:07 +0800

    init

---
+++ chatamz.js
@@ -0,0 +1,26 @@
+#!name=chatamz
+#!desc=chatamz
+
+[rewrite_local]
+chatamz = type=http-response, pattern=https:\/\/api\.ai520\.co\/chatapi\/\/member\/(\/wallet\/), script-path=https://raw.githubusercontent.com/Yu9191/Rewrite/main/gupaoyun.js, requires-body=true, max-size=-1, timeout=60
+
+[mitm]
+hostname = api.ai520.co
+*/
+if ($response.body) {
+    let responseBody = $response.body;
+    
+    let responseObject = JSON.parse(responseBody);
+    
+    if (responseObject.result && Array.isArray(responseObject.result)) {
+        responseObject.result.forEach(item => {
+            if (item.hasOwnProperty('availableValue')) {
+                item.availableValue = 2000;
+            }
+        });
+    }
+    
+    $done({ body: JSON.stringify(responseObject) });
+} else {
+    $done({});
+}
