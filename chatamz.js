ad6ce6b162278c6d402d8ace8803c9ed729f11c8
xuhao <535315941@qq.com>
2024-06-04 15:01:50 +0800

    123

--- chatamz.js
+++ chatamz.js
@@ -11,13 +11,10 @@ hostname = api.ai520.co
 if ($response.body) {
     let responseBody = $response.body;
     response.body = response.body.replace(/("aiModels"\s*:\s*\[10,\s*12,\s*13\])/, '$1, 20, 21');
-    responseBody = responseBody.replace(/("availableValue"\s*:\s*)\d+/g, '$112000');
+    responseBody = responseBody.replace(/("availableValue"\s*:\s*)\d+/g, '$11200');
     
     let responseObject = JSON.parse(responseBody);
     responseObject.message = "123445";
-    if (responseObject.result.groupInfo.title){
-      responseObject.result.groupInfo.title = "黄金会员";
-    }
      
     $done({ body: JSON.stringify(responseObject) });
 } else {
